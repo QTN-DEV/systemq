1ec247db01cc4f22fec1946492b625c0f07fc303
RealtaNua <nael.nathanael71@gmail.com>
2025-09-24 08:30:37 +0700

    feat: disable middleware

--- be/app/main.py
+++ be/app/main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router as api_router
 from app.db.beanie import lifespan_context
@@ -42,5 +43,12 @@ app = FastAPI(
     version="0.1.0",
     openapi_tags=TAGS_METADATA,
     lifespan=lifespan_context,
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 app.include_router(api_router)
