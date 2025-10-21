5391777dedca7b27cb74472d6944ef1ceee81392
QTN-DEV <dev@quantumteknologi.com>
2025-10-21 13:47:09 +0700

    change dockerfile

--- be/scripts/run_server.sh
+++ be/scripts/run_server.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 source "${PROJECT_ROOT}/.venv/bin/activate"
 
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 4
+exec uvicorn app.main:app --host 0.0.0.0 --port 47430 --workers 4
