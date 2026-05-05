ae4e6ecfdbac0a8e444049d4489b5a5f215316c9
Aldian Asmara <aldian.asmr@gmail.com>
2026-05-05 14:38:26 +0700

    refactor: remove junk tests

--- be/test_env.py
+++
@@ -1,4 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-print("KEY:", os.getenv("ANTHROPIC_API_KEY")[:10] if os.getenv("ANTHROPIC_API_KEY") else "None")
--- be/test_motor.py
+++
@@ -1,4 +0,0 @@
-from app.submodules.drive.documents import Documents
-print(dir(Documents))
-print(hasattr(Documents, "get_motor_collection"))
-print(hasattr(Documents, "get_settings"))
